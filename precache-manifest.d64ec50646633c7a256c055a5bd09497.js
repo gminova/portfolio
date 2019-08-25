10606591073883dce751baeabb64d77d86737314
gminova <minova1995@gmail.com>
2019-08-25 22:38:14 +0100

    Updates

---
+++ precache-manifest.d64ec50646633c7a256c055a5bd09497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b111a37852e448c1c3359e4a53d26053",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4a05f845d75847394e9c",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "21d374903c7e5529b265",
+    "url": "/portfolio/static/js/2.4b1ba94f.chunk.js"
+  },
+  {
+    "revision": "4a05f845d75847394e9c",
+    "url": "/portfolio/static/js/main.a4132514.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);

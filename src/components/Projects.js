05ab020daa1ec30e9a74be5bfb4a95aa04268a72
gminova <minova1995@gmail.com>
2019-08-27 00:33:52 +0100

    added project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,10 +3,18 @@ import React, { Component } from 'react';
 class Projects extends Component {
     render() {
         return (
-            <div className="section" id="Projects">
-                <h1>
-                    Projects
-        </h1>
+            <div className="section section--projects" id="Projects">
+                <div className="projects__gallery">
+                    <img className="projects__gallery__img" src="https://i.imgur.com/PIX7zzm.png" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/FoBXSDg.jpg" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/e0hBbEg.png" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/d4sSgKZ.png" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/RLAMVR2.png" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/uYnCglX.jpg" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/WRfEqwW.png" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/AOxnRcn.png" alt="projects__gallery__img"></img>
+                    <img className="projects__gallery__img" src="https://i.imgur.com/Bt2ZuXG.jpg" alt="projects__gallery__img"></img>
+                </div>
             </div>);
     }
 }

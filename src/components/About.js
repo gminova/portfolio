90fc7cd0849f4ee86d08006794e52d0d4e756a85
gminova <minova1995@gmail.com>
2019-08-26 22:56:09 +0100

    added tech stack images and responsive grid

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class About extends Component {
     render() {
         return (
@@ -11,12 +10,26 @@ class About extends Component {
                 <p>
                     Full-Stack JavaScript Developer
                 </p>
+                <div className="about__grid">
+                    <img className="logo" src="https://i.imgur.com/I7jXzxR.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/3HdTez6.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/q9Gd5B2.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/ZMOBeOz.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/whQw5pf.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/xFnEsQe.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/rpMby4E.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/L48CZMZ.jpg" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/HDh5HyN.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/sVtEvw8.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/h30QHHZ.png" alt="logo"></img>
+                    <img className="logo" src="https://i.imgur.com/DlDXScO.png" alt="logo"></img>
+                </div>
                 <a className="arrow__link" href="#Projects">
                     <p className="arrow">
                         &#x2193;
                         </p>
                 </a>
-            </div>);
+            </div >);
     }
 }
 

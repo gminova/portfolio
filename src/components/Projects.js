fec696faad995469c8c8aa41b37921612a1bcacb
gminova <minova1995@gmail.com>
2019-09-01 23:54:39 +0100

    reordered projects and links added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,15 +5,43 @@ class Projects extends Component {
         return (
             <div className="section section--projects" id="Projects">
                 <div className="projects__gallery">
-                    <img className="projects__gallery__img" src="https://i.imgur.com/PIX7zzm.png" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/FoBXSDg.jpg" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/e0hBbEg.png" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/d4sSgKZ.png" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/RLAMVR2.png" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/uYnCglX.jpg" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/WRfEqwW.png" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/AOxnRcn.png" alt="projects__gallery__img"></img>
-                    <img className="projects__gallery__img" src="https://i.imgur.com/Bt2ZuXG.jpg" alt="projects__gallery__img"></img>
+
+                    <a href="https://github.com/gminova/pearls-of-wisdom" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/WRfEqwW.png" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://github.com/gminova/crime-watcher" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/RLAMVR2.png" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://github.com/gminova/project-fear" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/AOxnRcn.png" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://github.com/gminova/to-do-app" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/Bt2ZuXG.jpg" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://gminova.github.io/calc-js/" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/PIX7zzm.png" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://github.com/gminova/search-gifs" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/FoBXSDg.jpg" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://github.com/gminova/tic-tac-toe" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/e0hBbEg.png" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://github.com/gminova/memory-game" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/d4sSgKZ.png" alt="projects__gallery__img"></img>
+                    </a>
+
+                    <a href="https://github.com/gminova/weather-app" target="_blank" rel="noopener noreferrer">
+                        <img className="projects__gallery__img" src="https://i.imgur.com/uYnCglX.jpg" alt="projects__gallery__img"></img>
+                    </a>
+
                 </div>
             </div>);
     }

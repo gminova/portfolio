8d22e89c65c895a7514139b2c5a2eaa6049cfeaa
Gigi Minova <minova.gigi@gmail.com>
2019-10-20 17:40:12 +0100

    add generation change project

--- src/model/data.js
+++ src/model/data.js
@@ -1,119 +1,135 @@
 export default {
-    //ABOUT
-    name: 'Gergana Minova',
-    profileImgSrc: 'https://i.imgur.com/wbshJ54.jpg',
-    aboutText: [
-        "I mentor prospect students at Founders and Coders. You can also find me at various meetups, such as London Node.js User Group, TAQFull(Technology, Agility & Quality), and London Tech Ladies.",
-        "I am committed to building software, tackling challenges and achieving goals. My objective is to join a friendly team and work towards clever and elegant solutions."
-    ],
-    aboutGrid: [
-        "https://i.imgur.com/I7jXzxR.png",
-        "https://i.imgur.com/3HdTez6.png",
-        "https://i.imgur.com/q9Gd5B2.png",
-        "https://i.imgur.com/ZMOBeOz.png",
-        "https://i.imgur.com/whQw5pf.png",
-        "https://i.imgur.com/xFnEsQe.png",
-        "https://i.imgur.com/rpMby4E.png",
-        "https://i.imgur.com/L48CZMZ.jpg",
-        "https://i.imgur.com/HDh5HyN.png",
-        "https://i.imgur.com/sVtEvw8.png",
-        "https://i.imgur.com/h30QHHZ.png",
-        "https://i.imgur.com/DlDXScO.png"
-    ],
-    //PROJECTS
-    projects: [
-        {
-            title: "Food Miles",
-            link: "https://github.com/gminova/food-miles",
-            img: "https://i.imgur.com/JbrkuvN.png",
-            text: "A full-stack group project using ExpressJS, HandlebarsJS, PostgreSQL, randomly generated dummy data, TDD, codecov, supertest, tape, Travis CI, SASS, and Heroku"
-        },
-        {
-            title: "Latest News",
-            link: "https://github.com/gminova/latest-news",
-            img: "https://github.com/gminova/latest-news/blob/master/public/images/Screenshot%20from%202019-09-05%2000-21-28.png?raw=true",
-            text: "A full-stack personal project using ExpressJS, HandlebarsJS, PostgreSQL, APIs, fetch, TDD, supertest, tape, authentication, JWT, Travis CI, CSS, BEM and Heroku"
-        },
-        {
-            title: "Crime Watcher",
-            link:"https://github.com/gminova/crime-watcher",
-            img:"https://i.imgur.com/RLAMVR2.png",
-            text: "A group project using the xhr module, callbacks, APIs, vanilla JavaScript, TDD, leaflet maps, Heroku, HTML and CSS"
-        },
-        {
-            title: "Pearls of wisdom",
-            link:"https://github.com/gminova/pearls-of-wisdom",
-            img:"https://i.imgur.com/WRfEqwW.png",
-            text: "A group project using ExpressJS, HandlebarsJS, PostgreSQL, tape, supertest, TDD, Heroku, CSS"
-        },
-        {
-            title: "Project Fear",
-            link:"https://github.com/gminova/project-fear",
-            img:"https://i.imgur.com/AOxnRcn.png",
-            text: "A group project using NodeJS, http and https, TDD, API, Travis CI, tape, supertest, nock, CSS and HTML"
-        },
-        {
-            title: "To-do app",
-            link: "https://github.com/gminova/to-do-app",
-            img: "https://i.imgur.com/Bt2ZuXG.jpg",
-            text: "A group project using HTML, CSS, vanilla JavaScript, and TDD"
-        },
-        {
-            title: "Weather app",
-            link: "https://github.com/gminova/weather-app",
-            img: "https://i.imgur.com/uYnCglX.jpg",
-            text: "A code-along team project using ExpressJS, API and EJS"
-        },
-        {
-            title: "Tic-tac-toe",
-            link: "https://github.com/gminova/tic-tac-toe",
-            img: "https://i.imgur.com/e0hBbEg.png",
-            text: "A code-along personal project following the React Documentation to help me learn React syntax, classes, inheritance and states"
-        },
-        {
-            title: "Calculator",
-            link: "https://gminova.github.io/calc-js/",
-            img: "https://i.imgur.com/PIX7zzm.png",
-            text: "A personal project using vanilla JavaScript, HTML and CSS"
-        },
-        {
-            title: "Search GIFs",
-            link: "https://github.com/gminova/search-gifs",
-            img: "https://i.imgur.com/FoBXSDg.jpg",
-            text: "A personal project using TDD, supertest, tape, TravisCI, nyc, codecov, Heroku, NodeJS and the Giphy API for searching gifs on the back-end"
-        },
-        {
-            title: "Japanese memory game",
-            link: "https://github.com/gminova/memory-game",
-            img: "https://i.imgur.com/d4sSgKZ.png",
-            text: "A code-along personal project using vanilla JavaScript, HTML and CSS"
-        }
-    ],
-    //CONTACT
-    social: [
-        {
-            name: 'Twitter',
-            url: "https://twitter.com/gigi_minova"
-        },
-        {   
-            name: 'GitHub',
-            url: "https://github.com/gminova"
-        },
-        {   
-            name:'LinkedIn',
-            url:"https://linkedin.com/in/gminova"
-        },
-        {
-            name: 'CodeWars',
-            url: "https://www.codewars.com/users/gminova"
-        },
-        {
-            name: 'FreeCodeCamp',
-            url: "https://www.freecodecamp.org/gminova"
-        }
-    ],
-    CV: {
-        name: 'Download CV.pdf',
-        url: "assets/GerganaMinovaCV.pdf"
+  //ABOUT
+  name: "Gergana Minova",
+  profileImgSrc: "https://i.imgur.com/wbshJ54.jpg",
+  aboutText: [
+    "I mentor prospect students at Founders and Coders. You can also find me at various meetups, such as London Node.js User Group, TAQFull(Technology, Agility & Quality), and London Tech Ladies.",
+    "I am committed to building software, tackling challenges and achieving goals. My objective is to join a friendly team and work towards clever and elegant solutions."
+  ],
+  aboutGrid: [
+    "https://i.imgur.com/I7jXzxR.png",
+    "https://i.imgur.com/3HdTez6.png",
+    "https://i.imgur.com/q9Gd5B2.png",
+    "https://i.imgur.com/ZMOBeOz.png",
+    "https://i.imgur.com/whQw5pf.png",
+    "https://i.imgur.com/xFnEsQe.png",
+    "https://i.imgur.com/rpMby4E.png",
+    "https://i.imgur.com/L48CZMZ.jpg",
+    "https://i.imgur.com/HDh5HyN.png",
+    "https://i.imgur.com/sVtEvw8.png",
+    "https://i.imgur.com/h30QHHZ.png",
+    "https://i.imgur.com/DlDXScO.png"
+  ],
+  //PROJECTS
+  projects: [
+    {
+      title: "Generation Change",
+      link: "https://github.com/fac-17/Generation-Change",
+      img: "https://i.imgur.com/KG96C3B.jpg",
+      text:
+        "A serverless team project build for a client using ReactJS, SASS, BEM, Airtable, Jest, Cypress, and Netlify. This website allows educators (e.g. , scout leaders) to find local social action projects for their young people."
+    },
+    {
+      title: "Food Miles",
+      link: "https://github.com/gminova/food-miles",
+      img: "https://i.imgur.com/JbrkuvN.png",
+      text:
+        "A full-stack group project using ExpressJS, HandlebarsJS, PostgreSQL, randomly generated dummy data, TDD, codecov, supertest, tape, Travis CI, SASS, and Heroku"
+    },
+    {
+      title: "Latest News",
+      link: "https://github.com/gminova/latest-news",
+      img:
+        "https://github.com/gminova/latest-news/blob/master/public/images/Screenshot%20from%202019-09-05%2000-21-28.png?raw=true",
+      text:
+        "A full-stack personal project using ExpressJS, HandlebarsJS, PostgreSQL, APIs, fetch, TDD, supertest, tape, authentication, JWT, Travis CI, CSS, BEM and Heroku"
+    },
+    {
+      title: "Crime Watcher",
+      link: "https://github.com/gminova/crime-watcher",
+      img: "https://i.imgur.com/RLAMVR2.png",
+      text:
+        "A group project using the xhr module, callbacks, APIs, vanilla JavaScript, TDD, leaflet maps, Heroku, HTML and CSS"
+    },
+    {
+      title: "Pearls of wisdom",
+      link: "https://github.com/gminova/pearls-of-wisdom",
+      img: "https://i.imgur.com/WRfEqwW.png",
+      text:
+        "A group project using ExpressJS, HandlebarsJS, PostgreSQL, tape, supertest, TDD, Heroku, CSS"
+    },
+    {
+      title: "Project Fear",
+      link: "https://github.com/gminova/project-fear",
+      img: "https://i.imgur.com/AOxnRcn.png",
+      text:
+        "A group project using NodeJS, http and https, TDD, API, Travis CI, tape, supertest, nock, CSS and HTML"
+    },
+    {
+      title: "To-do app",
+      link: "https://github.com/gminova/to-do-app",
+      img: "https://i.imgur.com/Bt2ZuXG.jpg",
+      text: "A group project using HTML, CSS, vanilla JavaScript, and TDD"
+    },
+    {
+      title: "Weather app",
+      link: "https://github.com/gminova/weather-app",
+      img: "https://i.imgur.com/uYnCglX.jpg",
+      text: "A code-along team project using ExpressJS, API and EJS"
+    },
+    {
+      title: "Tic-tac-toe",
+      link: "https://github.com/gminova/tic-tac-toe",
+      img: "https://i.imgur.com/e0hBbEg.png",
+      text:
+        "A code-along personal project following the React Documentation to help me learn React syntax, classes, inheritance and states"
+    },
+    {
+      title: "Calculator",
+      link: "https://gminova.github.io/calc-js/",
+      img: "https://i.imgur.com/PIX7zzm.png",
+      text: "A personal project using vanilla JavaScript, HTML and CSS"
+    },
+    {
+      title: "Search GIFs",
+      link: "https://github.com/gminova/search-gifs",
+      img: "https://i.imgur.com/FoBXSDg.jpg",
+      text:
+        "A personal project using TDD, supertest, tape, TravisCI, nyc, codecov, Heroku, NodeJS and the Giphy API for searching gifs on the back-end"
+    },
+    {
+      title: "Japanese memory game",
+      link: "https://github.com/gminova/memory-game",
+      img: "https://i.imgur.com/d4sSgKZ.png",
+      text:
+        "A code-along personal project using vanilla JavaScript, HTML and CSS"
     }
-}
+  ],
+  //CONTACT
+  social: [
+    {
+      name: "Twitter",
+      url: "https://twitter.com/gigi_minova"
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/gminova"
+    },
+    {
+      name: "LinkedIn",
+      url: "https://linkedin.com/in/gminova"
+    },
+    {
+      name: "CodeWars",
+      url: "https://www.codewars.com/users/gminova"
+    },
+    {
+      name: "FreeCodeCamp",
+      url: "https://www.freecodecamp.org/gminova"
+    }
+  ],
+  CV: {
+    name: "Download CV.pdf",
+    url: "assets/GerganaMinovaCV.pdf"
+  }
+};

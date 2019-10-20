1d86ffbd4bb0f16a82c6505b4d35ebcc6827402a
Gigi Minova <minova.gigi@gmail.com>
2019-10-20 17:43:07 +0100

    fix typo

--- src/model/data.js
+++ src/model/data.js
@@ -27,7 +27,7 @@ export default {
       link: "https://github.com/fac-17/Generation-Change",
       img: "https://i.imgur.com/KG96C3B.jpg",
       text:
-        "A serverless team project build for a client using ReactJS, SASS, BEM, Airtable, Jest, Cypress, and Netlify. This website allows educators (e.g. , scout leaders) to find local social action projects for their young people."
+        "A serverless team project build for a client using ReactJS, SASS, BEM, Airtable, Jest, Cypress, and Netlify. This website allows educators (e.g. teachers, scout leaders) to find local social action projects for their young people."
     },
     {
       title: "Food Miles",

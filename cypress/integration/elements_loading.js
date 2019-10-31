ba5e16b683587e69695daab99018c2e24e4437b5
Gigi Minova <minova.gigi@gmail.com>
2019-10-31 12:33:20 +0000

    check scroww down arrow click even

--- cypress/integration/elements_loading.js
+++ cypress/integration/elements_loading.js
@@ -3,9 +3,19 @@ describe("Site Navigation", function() {
     cy.visit("http://localhost:3000");
   });
 
-  it("Get about header", function() {
+  it("Gets about header", function() {
     cy.get(".about__header").should("contain", "Gergana Minova");
     expect(".about__header").to.be.a("string");
+  });
+
+  it("Gets menu icon", function() {
     cy.get(".menu__button").should("contain", "☰");
+  });
+
+  it("Scrolls to projects on click of arrow down button", function() {
+    cy.get(".arrow")
+      .should("contain", "↓")
+      .click({ force: true });
+    cy.url().should("eq", "http://localhost:3000/#Projects");
   });
 });

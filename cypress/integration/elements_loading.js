daba0eb7a1de6e11efd5910e617e99dadb559ae1
Gigi Minova <minova.gigi@gmail.com>
2019-10-31 14:38:46 +0000

    test scrolling menu function

--- cypress/integration/elements_loading.js
+++ cypress/integration/elements_loading.js
@@ -18,4 +18,37 @@ describe("Site Navigation", function() {
       .click({ force: true });
     cy.url().should("eq", "http://localhost:3000/#Projects");
   });
+
+  it("Scrolls to projects on click of arrow down button", function() {
+    cy.get(".menu__content")
+      .invoke("show")
+      .click();
+    cy.get(".testid-about").should("be.visible");
+    cy.get(".testid-about").should("contain", "About");
+    cy.get(".testid-about").click();
+    cy.url().should("eq", "http://localhost:3000/#About");
+    cy.get(".menu__content").invoke("hide");
+  });
+
+  it("Scrolls to projects on click of arrow down button", function() {
+    cy.get(".menu__content")
+      .invoke("show")
+      .click();
+    cy.get(".testid-projects").should("be.visible");
+    cy.get(".testid-projects").should("contain", "Projects");
+    cy.get(".testid-projects").click();
+    cy.url().should("eq", "http://localhost:3000/#Projects");
+    cy.get(".menu__content").invoke("hide");
+  });
+
+  it("Scrolls to projects on click of arrow down button", function() {
+    cy.get(".menu__content")
+      .invoke("show")
+      .click();
+    cy.get(".testid-contact").should("be.visible");
+    cy.get(".testid-contact").should("contain", "Contact");
+    cy.get(".testid-contact").click();
+    cy.url().should("eq", "http://localhost:3000/#Contact");
+    cy.get(".menu__content").invoke("hide");
+  });
 });
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Navbar extends Component {
-    render() {
-        return (
-            <div className="menu">
-                <button className="menu__button">
-                    &#9776;
-                    </button>
-                <div className="menu__content">
-                    <a className="menu__content__link" href="#About" rel="noopener noreferrer">
-                        About
-                    </a>
-                    <a className="menu__content__link" href="#Projects" rel="noopener noreferrer">
-                        Projects
-                    </a>
-                    <a className="menu__content__link" href="#Contact" rel="noopener noreferrer">
-                        Contact
-                    </a>
-                </div>
-            </div>);
-    }
+  render() {
+    return (
+      <div className="menu">
+        <button className="menu__button">&#9776;</button>
+        <div className="menu__content">
+          <a
+            className="menu__content__link testid-about"
+            href="#About"
+            rel="noopener noreferrer"
+          >
+            About
+          </a>
+          <a
+            className="menu__content__link testid-projects"
+            href="#Projects"
+            rel="noopener noreferrer"
+          >
+            Projects
+          </a>
+          <a
+            className="menu__content__link testid-contact"
+            href="#Contact"
+            rel="noopener noreferrer"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Navbar;

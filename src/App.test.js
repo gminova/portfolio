951c6135a88e81a98b73d6a958adcd84cf55aa9d
Gigi Minova <minova.gigi@gmail.com>
2019-11-03 19:53:16 +0000

    remove snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import renderer from 'react-test-renderer';
-
-
-it('renders correctly when there are no items', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
